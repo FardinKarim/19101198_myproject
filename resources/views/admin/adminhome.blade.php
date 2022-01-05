5a7d7aace77a13515cb1ac85d8d742417fa5c1e5
FardinKarim <fardin.junayed.karim@g.bracu.ac.bd>
2022-01-06 00:10:18 +0600

    initial commit

---
+++ resources/views/admin/adminhome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin</title>
+</head>
+<body>
+
+<x-app-layout>
+
+</x-app-layout>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  @include("admin.admincss")
+  </head>
+  <body>
+   @include("admin.navbar") 
+        <!-- main-panel ends -->
+   
+   @include("admin.adminscript")
+  </body>
+  </html>
+</body>  
+
+</html>
